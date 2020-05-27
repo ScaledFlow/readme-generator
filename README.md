bbee5dcf1da3c98d648339b5184fcec2589d66a3
John Leintz <johnleintz@scaledflow.com>
2020-05-27 12:06:43 -0500

    refactored index.js

--- README.md
+++
@@ -1,5 +0,0 @@
-# This is the title02
-
-![APM](https://img.shields.io/apm/l/vim-mode)
-
-[![Build Status](https://travis-ci.com/ScaledFlow/readme-generator.svg?branch=master)](https://travis-ci.com/ScaledFlow/readme-generator)
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-var fs = require("fs");
-var inquirer = require("inquirer");
+const fs = require("fs");
+const inquirer = require("inquirer");
+const util = require("util");
 
-inquirer
-  .prompt([
+const writeFileAsync = util.promisify(fs.writeFile);
+
+let badges =
+  "![APM](https://img.shields.io/apm/l/vim-mode) [![Build Status](https://travis-ci.com/ScaledFlow/readme-generator.svg?branch=master)](https://travis-ci.com/ScaledFlow/readme-generator)";
+
+function promptUser() {
+  return inquirer.prompt([
     {
       type: "input",
       message: "Enter project title:",
@@ -13,51 +19,62 @@ inquirer
       message: "Enter project description:",
       name: "Description",
     },
-    {
-      type: "input",
-      message: "Enter installation notes:",
-      name: "Installation",
-    },
-    {
-      type: "input",
-      message: "Enter usage notes:",
-      name: "Usage",
-    },
-    {
-      type: "input",
-      message: "Enter License info:",
-      name: "License",
-    },
-    {
-      type: "input",
-      message: "Enter contributers:",
-      name: "Contributing",
-    },
-    {
-      type: "input",
-      message: "Enter testing info:",
-      name: "Testing",
-    },
-    {
-      type: "input",
-      message: "Enter contributers",
-      name: "Contributing",
-    },
-    {
-      type: "input",
-      message: "Enter questions:",
-      name: "Questions",
-    },
-  ])
-  .then(function (response) {
-    console.log(response.Questions);
+    // {
+    //   type: "input",
+    //   message: "Enter installation notes:",
+    //   name: "Installation",
+    // },
+    // {
+    //   type: "input",
+    //   message: "Enter usage notes:",
+    //   name: "Usage",
+    // },
+    // {
+    //   type: "input",
+    //   message: "Enter License info:",
+    //   name: "License",
+    // },
+    // {
+    //   type: "input",
+    //   message: "Enter contributers:",
+    //   name: "Contributing",
+    // },
+    // {
+    //   type: "input",
+    //   message: "Enter testing info:",
+    //   name: "Testing",
+    // },
+    // {
+    //   type: "input",
+    //   message: "Enter contributers",
+    //   name: "Contributing",
+    // },
+    // {
+    //   type: "input",
+    //   message: "Enter questions:",
+    //   name: "Questions",
+    // },
+  ]);
+}
 
-    let buildTitle = "# " + response.Title;
+function generateREADME(answers) {
+  return `test within generated`;
+}
 
-    fs.writeFile("README.md", buildTitle, function (err) {
-      if (err) {
-        return console.log(err);
-      }
-      console.log("README Created!");
-    });
-  });
+async function init() {
+  console.log("made it");
+  try {
+    const answers = await promptUser();
+    console.log(answers);
+    console.log(answers.Title);
+    const md = generateREADME(answers);
+    console.log("generateREADME " + md);
+
+    await writeFileAsync("README.md", generateREADME() + "\n");
+    console.log("Successfully wrote to README.md");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+init();
